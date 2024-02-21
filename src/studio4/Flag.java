dbcfed01a167e91e951566609c295e4012bbc4a4
andrewbear <andrewbear@localhost>
2024-02-21 15:30:34 -0600

    laclede

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,11 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledRectangle(0.5, 0.5, 0.25, 0.175);
+		StdDraw.setPenColor(StdDraw.WHITE);
+		StdDraw.filledCircle(0.5, 0.5, .145);
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledCircle(0.5, 0.5, .125);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,48 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		
+		if (shape.equals("rectangle")) {
+			StdDraw.setPenColor(in.nextInt(),in.nextInt(),in.nextInt());
+			if(in.nextBoolean()==true) {
+				StdDraw.filledRectangle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+			else {
+				StdDraw.rectangle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+		}
+		if (shape.equals("triangle")) {
+			StdDraw.setPenColor(in.nextInt(),in.nextInt(),in.nextInt());
+			if(in.nextBoolean()==true) {
+				double[] x = new double [3];
+				double[] y = new double [3];
+				for (int i =0;i<3;i++) {
+					x[i]=in.nextDouble();
+					y[i]=in.nextDouble();
+				}
+				StdDraw.filledPolygon(x, y);
+			}
+			
+			else {
+				double[] x = new double [3];
+				double[] y = new double [3];
+				for (int i =0;i<3;i++) {
+					x[i]=in.nextDouble();
+					y[i]=in.nextDouble();
+				}
+				StdDraw.polygon(x, y);
+			
+		}}
+		if (shape.equals("ellipse")) {
+			StdDraw.setPenColor(in.nextInt(),in.nextInt(),in.nextInt());
+			if(in.nextBoolean()==true) {
+				StdDraw.filledEllipse(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+			else {
+				StdDraw.ellipse(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+		
+		}
 	}
-}
+	}
